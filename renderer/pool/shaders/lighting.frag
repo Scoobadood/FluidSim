1d1038488d7009c76ff44b9c84892f465fcc3aff
Dave <scoobadood@me.com>
2024-01-01 16:37:31 +1100

    WIP: Generate bar chart data

--- renderer/pool/shaders/lighting.frag
+++ renderer/pool/shaders/lighting.frag
@@ -5,12 +5,33 @@ layout (location=0) out vec4 frag_colour;
 in vec3 frag_normal;
 in vec3 frag_position;
 
-uniform vec3 object_color;
-uniform vec3 light_color;
+uniform float kd;               // Diffuse constant
+uniform float ka;               // Ambient constant
+uniform float ks;               // Specular constant
+uniform float alpha;            // Shininess of surface
+
+uniform vec3 light_dir;         // From light. Invert for dir to light
+uniform float light_intensity;
+
+uniform vec3 object_colour;
 
 void main() {
-    float ambientStrength = 0.9;
-    vec3 ambient = ambientStrength * light_color;
-    vec3 result = ambient * object_color;
-    frag_colour = vec4(result, 1.0);
+    vec3 n_n           = normalize(frag_normal);
+    vec3 n_ld          = normalize(-light_dir);
+
+    // Calc diffuse lighting geometry component
+    float n_dot_w      = max(0, dot(n_ld, n_n));
+
+    // Calc specular lighting Phong
+    vec3 look          = -normalize(frag_position);
+    vec3 r             = -reflect(n_ld, n_n);
+    float r_dot_v      = max(0, dot(r, look));
+    float spec_coeff   = pow(r_dot_v, alpha);
+
+    vec3 spec_light    = ks * spec_coeff * light_intensity * vec3(1,1,1);
+    vec3 ambient_light = ka * light_intensity * object_colour;
+    vec3 diff_light    = kd * n_dot_w * light_intensity * object_colour;
+    vec3 l1 = diff_light + spec_light;
+
+    frag_colour = vec4(l1, 1.0);
 }
--- renderer/pool/src/main.cc
+++ renderer/pool/src/main.cc
@@ -9,8 +9,42 @@
 #include "gl_common.h"
 #include "shader.h"
 
+/*
+ *     // Left face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, -1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, -1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, -1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, -1, 0, 0});
+    // Front face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, 0, 0, -1});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 0, 0, -1});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 0, 0, -1});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, 0, 0, -1});
+    // Right face
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 1, 0, 0});
+    // Back face
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 0, 0, 1});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, 0, 0, 1});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, 0, 0, 1});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 0, 0, 1});
+    // Top face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, 0, 1, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 0, 1, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 0, 1, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, 0, 1, 0});
+    // Bottom face
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, 0, -1, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 0, -1, 0});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 0, -1, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, 0, -1, 0});
+
+ */
 
 const int32_t POS_ATTR = 0;
+const int32_t NORM_ATTR = 1;
 
 /* ******************************************************************************************
  *  Error macro
@@ -69,35 +103,117 @@ GLFWwindow *initialise() {
 }
 
 /* ******************************************************************************************
+ *  Generate the geometry for the scene.
+ *  We will have N columns, each of which is WxD cross section and H[n] high
+ *  The columns will be adjacent and centred on the origin.
+ * ******************************************************************************************/
+void generate_geometry(uint32_t num_columns, float width, float depth, const std::vector<float> &heights,
+                       std::vector<float>& vertex_data,
+                       std::vector<uint32_t>& indices) {
+  auto total_width = (float)num_columns * width;
+  const auto min_y = 0.0f;
+  const auto min_z = -(depth / 2.0f);
+  const auto max_z = (depth / 2.0f);
+  auto min_x = -(total_width / 2.0f);
+
+  uint32_t base_indices[36] = {0, 1, 2,
+                             0, 2, 3,
+                             4, 5, 6,
+                             4, 6, 7,
+                             8, 9, 10,
+                             8, 10, 11,
+                             12, 13, 14,
+                             12, 14, 15,
+                             16, 17, 18,
+                             16, 18, 19,
+                             20, 21, 22,
+                             20, 22, 23
+  };
+
+  auto base_vertex = 0;
+  for (auto i = 0; i < num_columns; ++i) {
+    auto max_x = min_x + width;
+    auto max_y = heights[i];
+
+    // Left face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, -1.0f, 0.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, -1.0f, 0.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, -1, 0, 0});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, -1, 0, 0});
+    // Front face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, 0.0f, 0.0f, -1.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 0.0f, 0.0f, -1.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 0.0f, 0.0f, -1.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, 0.0f, 0.0f, -1.0f});
+    // Right face
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 1.0f, 0.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 1.0f, 0.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 1.0f, 0.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 1.0f, 0.0f, 0.0f});
+    // Back face
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 0.0f, 0.0f, 1.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, 0.0f, 0.0f, 1.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, 0.0f, 0.0f, 1.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 0.0f, 0.0f, 1.0f});
+    // Top face
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, max_z, 0.0f, 1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, max_z, 0.0f, 1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, max_y, min_z, 0.0f, 1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, max_y, min_z, 0.0f, 1.0f, 0.0f});
+    // Bottom face
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, min_z, 0.0f, -1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, min_z, 0.0f, -1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {max_x, min_y, max_z, 0.0f, -1.0f, 0.0f});
+    vertex_data.insert(vertex_data.end(), {min_x, min_y, max_z, 0.0f, -1.0f, 0.0f});
+
+
+    for(unsigned int base_index : base_indices) {
+      indices.push_back(base_index+base_vertex);
+    }
+    base_vertex+=24;
+
+    min_x += width;
+  }
+
+}
+
+/* ******************************************************************************************
  *  Create the geometry for the scene
  * ******************************************************************************************/
 void create_geometry(GLuint &vao, GLuint &vbo, GLuint &ebo, GLsizei &num_elements) {
   spdlog::info("Creating geometry");
 
+  std::vector<float> vertex_data;
+  std::vector<uint32_t> index_data;
+  generate_geometry(5, 0.25f, 0.25f, {1.0f, 0.8f, 0.6f, 0.7f, 0.3f}, vertex_data, index_data);
+
   // VAO
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
 
-  auto vtx_sz = 12; // No normals or textures yet
-  auto num_vertices = sizeof(CUBE_VERTEX_DATA) / 3;
-  num_elements = sizeof(CUBE_INDICES);
+  auto bytes_per_vertex = 4 * (3 + 3); // No textures yet
+  num_elements = index_data.size();
 
   // Vertex locations
   glGenBuffers(1, &vbo);
   glGenBuffers(1, &ebo);
 
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, vtx_sz * num_vertices, CUBE_VERTEX_DATA, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, vertex_data.size() * sizeof(float ), vertex_data.data(), GL_STATIC_DRAW);
   glEnableVertexAttribArray(POS_ATTR);
-  glVertexAttribPointer(POS_ATTR, 3, GL_FLOAT, GL_FALSE, vtx_sz, (GLvoid *)
+  glVertexAttribPointer(POS_ATTR, 3, GL_FLOAT, GL_FALSE, bytes_per_vertex, (GLvoid *)
           nullptr);
+
+  glEnableVertexAttribArray(NORM_ATTR);
+  glVertexAttribPointer(NORM_ATTR, 3, GL_FLOAT, GL_FALSE, bytes_per_vertex, (GLvoid *)
+          12);
 
 // Normals and textures
 ///    glEnableVertexAttribArray(tx_attr);
 //    glVertexAttribPointer(tx_attr, 2, GL_FLOAT, GL_FALSE, vtx_sz, (GLvoid *) 24);
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, num_elements, CUBE_INDICES, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_data.size() * sizeof (uint32_t), index_data.data(), GL_STATIC_DRAW);
 
   CHECK_GL_ERROR("create geometry");
 }
@@ -127,10 +243,12 @@ int main(int argc, char *argv[]) {
   //
   // Create a shader
   //
-  auto shader =   Shader::from_files("/Users/dave/Projects/FluidSim/renderer/pool/shaders/lighting.vert",
+  auto shader = Shader::from_files("/Users/dave/Projects/FluidSim/renderer/pool/shaders/lighting.vert",
                                    "/Users/dave/Projects/FluidSim/renderer/pool/shaders/lighting.frag");
   CHECK_GL_ERROR("Create shader");
 
+  auto angle = 0.0f;
+  auto delta_angle = 0.01f;
   while (!glfwWindowShouldClose(window)) {
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
@@ -153,23 +271,35 @@ int main(int argc, char *argv[]) {
 
     // Update view
     glm::mat4 view{1};
-    view = glm::translate(view, glm::vec3(0, 3, -15));
+    view = glm::translate(view, glm::vec3(0, -3, -15));
     view = glm::rotate(view, (float) M_PI_2, glm::vec3(0, 1, 0));
+    glm::mat4 model = glm::mat4(1.0);
+//  model = glm::translate(model,glm::vec3(0,0,-10));
+    model = glm::rotate(model, angle, glm::vec3(0, 1, 0));
+    angle += delta_angle;
 
     shader->use();
     shader->set_uniform("project", project);
     shader->set_uniform("view", view);
-    shader->set_uniform("light_color", glm::vec3{1, 1, 1});
-    shader->set_uniform("object_color", glm::vec3{0.7f, 0.6f, 0.5f});
-
-    glm::mat4 model = glm::mat4(1.0);
-//  model = glm::translate(model,glm::vec3(0,0,-10));
-//  model = glm::rotate(model, head_angle_[i], glm::vec3(0, 1, 0));
     shader->set_uniform("model", model);
-//  shader_->set_uniform("alpha", head_alpha_[i]);
+
+    shader->set_uniform("light_dir", glm::vec3(10, -10, 0));
+    shader->set_uniform("light_intensity", 1.0f);
+
+    shader->set_uniform("kd", 0.8f);
+    shader->set_uniform("ks", 0.5f);
+    shader->set_uniform("ka", 0.1f);
+    shader->set_uniform("alpha", 10.0f);
+    shader->set_uniform("object_colour", glm::vec3{1.0f, 0.6f, 0.0f});
+
 
     glDrawElements(GL_TRIANGLES, num_elements, GL_UNSIGNED_INT, (void *) nullptr);
     CHECK_GL_ERROR("Render");
+
+    //
+    // Update geometry
+    //
+
 
     //
     // End of frame
