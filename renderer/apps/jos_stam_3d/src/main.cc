cd900d1c9ad9afc0e1f9034e118741f34c51313c
Dave <scoobadood@me.com>
2024-01-10 08:26:15 +1100

    WIP: Added comments

--- renderer/apps/jos_stam_3d/src/main.cc
+++ renderer/apps/jos_stam_3d/src/main.cc
@@ -7,7 +7,10 @@
 #include <common/common.h>
 #include <glfw_utils/window.h>
 
-void clear_window( Window &window) {
+/*
+ * Clear render viewport for next pass
+ */
+void clear_window(Window &window) {
   int width, height;
   window.GetFrameBufferSize(width, height);
   glViewport(0, 0, width, height);
@@ -20,12 +23,11 @@ void clear_window( Window &window) {
 int main(int argc, char *argv[]) {
   spdlog::cfg::load_env_levels();
 
-  Window window(800, 600, "Party");
+  Window window(800, 600, "Stam3D");
 //  g_arcball = new ArcBall(800, 600);
 //  setup_arcball(window);
 
-  glEnable(GL_PROGRAM_POINT_SIZE);
-
+// We use a basic texture shader to render the image as a quad
   auto shader = Shader::from_files(//
       "/Users/dave/Projects/FluidSim/renderer/apps/jos_stam_3d/shaders/basic.vert",
       "/Users/dave/Projects/FluidSim/renderer/apps/jos_stam_3d/shaders/basic.frag");
@@ -44,11 +46,11 @@ int main(int argc, char *argv[]) {
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   float verts[] = {-1, 1, 0, 0, 1,
-                   1, 1, 0,1,1,
-                   1, -1, 0,1,0,
-                   -1, 1, 0,0,1,
-                   1, -1, 0,1,0,
-                   -1, -1, 0,0,0
+                   1, 1, 0, 1, 1,
+                   1, -1, 0, 1, 0,
+                   -1, 1, 0, 0, 1,
+                   1, -1, 0, 1, 0,
+                   -1, -1, 0, 0, 0
   };
   glBufferData(GL_ARRAY_BUFFER, 6 * 5 * sizeof(float), verts, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
@@ -65,17 +67,19 @@ int main(int argc, char *argv[]) {
   const int GRID_SIZE = 64;
   auto sim = std::make_shared<GridFluidSimulator3D>(GRID_SIZE, 0.2f);
   sim->Initialise();
-  sim->AddSource(GRID_SIZE/2, GRID_SIZE-2, 1.0f,0.0, -3.0f);
-  sim->AddSource(1, GRID_SIZE/2, 1.0f,5.0, 0.0f);
+  sim->AddSource(GRID_SIZE / 2, GRID_SIZE - 2, 1.0f, 0.0, -3.0f);
+  sim->AddSource(1, GRID_SIZE / 2, 1.0f, 5.0, 0.0f);
 
   /* ************************************************************************************************
- * **
- * **   Create an image and load the data to a texture
- * **
- * ************************************************************************************************/
-  std::vector<uint8_t> image_data(GRID_SIZE * GRID_SIZE * 3, 0);
-
+   * **
+   * **   Make a texture to hold the render
+   * **
+   * ************************************************************************************************/
   auto texture = std::make_shared<Texture>(GRID_SIZE, GRID_SIZE);
+
+
+  // Allocate image data to which to render.
+  std::vector<uint8_t> image_data(GRID_SIZE * GRID_SIZE * 3, 0);
 
   glm::mat4 r(1);
 //  r = glm::rotate_slow(r,(float)M_PI/2.0f, glm::vec3{0,0,1});
@@ -91,19 +95,25 @@ int main(int argc, char *argv[]) {
     glm::mat4 project = glm::perspective(glm::radians(35.0f), ratio, 0.1f, 1000.0f);
 
 
-    //
-    //
-    //
+    /* ************************************************************************************************
+     * **
+     * **   Render the fluid sim into the image data and load to texture
+     * **
+     * ************************************************************************************************/
     auto data = sim->Density();
     image_data.clear();
-    for( auto d : data) {
-      image_data.push_back((uint8_t)(std::roundf(d * 255.0f)));
-      image_data.push_back((uint8_t)(std::roundf(d * 255.0f)));
-      image_data.push_back((uint8_t)(std::roundf(d * 255.0f)));
+    for (auto d : data) {
+      image_data.push_back((uint8_t) (std::roundf(d * 255.0f)));
+      image_data.push_back((uint8_t) (std::roundf(d * 255.0f)));
+      image_data.push_back((uint8_t) (std::roundf(d * 255.0f)));
     }
     texture->SetImageData(image_data.data());
 
-
+    /* ************************************************************************************************
+     * **
+     * **   Draw the texture
+     * **
+     * ************************************************************************************************/
     shader->use();
     shader->set_uniform("project", project);
     glm::mat4 view{1};
@@ -113,12 +123,15 @@ int main(int argc, char *argv[]) {
     shader->set_uniform("model", glm::mat4(1));
     texture->BindToTextureUnit(0);
     shader->set_uniform("tx", 0);
-
-
     glDrawArrays(GL_TRIANGLES, 0, 6);
     CHECK_GL_ERROR("Draw")
 
-    // Check elapsed time
+
+    /* ************************************************************************************************
+     * **
+     * **   Udpate the physics sim.
+     * **
+     * ************************************************************************************************/
     auto elapsed = timer->ElapsedTime();
     sim->Simulate(elapsed);
 
